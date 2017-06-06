e79b0b856f491404866e55fce8ca51441349ffca
jose.mario <jose.mario@nddigital.com.br>
2017-06-06 10:38:51 -0300

    menu

---
+++ Essencial/UIView.swift
@@ -0,0 +1,20 @@
+//
+//  UIView.swift
+//  Essencial
+//
+//  Created by Jose Mario Fernandes on 6/6/17.
+//  Copyright © 2017 Jose Mario Fernandes. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    class func loadNib<T: UIView>(_ viewType: T.Type) -> T {
+        let className = String.className(viewType)
+        return Bundle(for: viewType).loadNibNamed(className, owner: nil, options: nil)!.first as! T
+    }
+    
+    class func loadNib() -> Self {
+        return loadNib(self)
+    }
+}
